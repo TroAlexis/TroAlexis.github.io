651bd3b9356e386b30dd2b097441889dcb6d712b
TroAlexis <aleksei.troshin1997@gmail.com>
2020-06-25 10:59:09 +0300

    Added checkbox, masked input, radio and toggle buttons.

---
+++ modules/forms/text-field/__masked/__masked.js
@@ -0,0 +1,8 @@
+
+//REQUIRES FORMATTER.JS
+
+
+var masked = new Formatter(document.getElementById('date'), {
+    'pattern' : '{{99}}.{{99}}.{{9999}}',
+    'persistent' : false
+})
---
+++ test.js
@@ -0,0 +1,4 @@
+var masked = new Formatter(document.getElementById('date'), {
+    'pattern' : '{{99}}.{{99}}.{{9999}}',
+    'persistent' : false
+})
