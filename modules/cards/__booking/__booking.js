0cfb8dfb482939d24d6b724025cfa92ef2c7c389
TroAlexis <aleksei.troshin1997@gmail.com>
2020-07-24 11:01:13 +0300

    Refactored booking.js code.

--- modules/cards/__booking/__booking.js
+++ modules/cards/__booking/__booking.js
@@ -1,61 +1,74 @@
-// CREATE BOOKING OBJECT;
-let booking = {
-    'card' : bookingCard = document.querySelector('.booking'),
-    'arrival' : '',
-    'departure' : '',
-    'guests' : 0,
-    'cost' : Number(bookingCard.querySelector('.booking__cost').firstChild.textContent.match(/(.+)₽/)[1].replace(/\s/g, '')),
-    'extras' : Number(bookingCard.querySelector('.booking__extra').lastChild.previousSibling.textContent.slice(0,-1)),
-    'discount' : Number(bookingCard.querySelector('.booking__services p').textContent.match(/скидка\s(.+)₽/)[1].replace(/\s/g, '')),
-    'updateGuests' : function (bookingCard) {
-        const regex = new RegExp('\\d+');
-        let found = bookingCard.querySelector('.dropdown--guests .dropdown__select span').textContent.match(regex);
-        this.guests = (found) ? Number(found[0]) : 0;
-    },
-    'getTotalCost' : function (days) {
-        return (this.cost*days) + this.extras;
-    },
-    'getTotalDays' : function () {
-        return (this.departure - this.arrival)/(1000*60*60*24);
+{
+// CREATE DOMSTRINGS
+    const DOM = {
+        booking: '.booking',
+        cost: '.booking__cost',
+        extra: '.booking__extra',
+        discount: '.booking__services p',
+        guests: '.dropdown--guests .dropdown__select span',
+        guestsApply: '.dropdown--guests .dropdown__content .dropdown__apply',
+        dateApply: '.dropdown--date .dropdown__content .dropdown__apply',
+        days: '.booking .booking__days',
+        sumOfDays: '.booking__sum-total',
+        total: '.booking__total'
     }
-}
+// CREATE BOOKING OBJECT;
+    let booking = {
+        card: bookingCard = document.querySelector(DOM.booking),
+        arrival: '',
+        departure: '',
+        guests: 0,
+        cost: Number(bookingCard.querySelector(DOM.cost).firstChild.textContent.match(/(.+)₽/)[1].replace(/\s/g, '')),
+        extras: Number(bookingCard.querySelector(DOM.extra).lastChild.previousSibling.textContent.slice(0, -1)),
+        discount: Number(bookingCard.querySelector(DOM.discount).textContent.match(/скидка\s(.+)₽/)[1].replace(/\s/g, '')),
+        updateGuests: function (bookingCard) {
+            const regex = new RegExp('\\d+');
+            let found = bookingCard.querySelector(DOM.guests).textContent.match(regex);
+            this.guests = (found) ? Number(found[0]) : 0;
+        },
+        getTotalCost: function (days) {
+            return (this.cost * days) + this.extras;
+        },
+        getTotalDays: function () {
+            return (this.departure - this.arrival) / (1000 * 60 * 60 * 24);
+        },
+        setEventListeners: function () {
+            const guestsApply = this.card.querySelector(DOM.guestsApply);
+            guestsApply.addEventListener('click', () => this.updateGuests(this.card))
+
+            const dateApply = this.card.querySelector(DOM.dateApply);
+            const bookingDays = this.card.querySelector(DOM.days);
+            const bookingSumDays = this.card.querySelector(DOM.sumOfDays);
+            const overAll = this.card.querySelector(DOM.total).lastElementChild;
+            const regExpDays = new RegExp('\\d+\\sсут[а-яё]+');
+            dateApply.addEventListener('click', () => {
+                this.arrival = new Date(Number(arrival[2]), Number(arrival[1]), Number(arrival[0]));
+                this.departure = new Date(Number(departDate[2]), Number(departDate[1]), Number(departDate[0]));
+                if (this.getTotalDays() > 0) {
+                    bookingDays.textContent = bookingDays.textContent.replace(regExpDays, ' ' + this.getTotalDays() + ' ' + getWordForm(this.getTotalDays(), 'сутки', 'суток', 'суток'));
+                    bookingSumDays.textContent = spacify(this.cost * this.getTotalDays()) + '₽';
+                    overAll.textContent = spacify(Number(this.getTotalCost(this.getTotalDays())) - this.discount) + '₽';
+                }
+            })
+        }
+    }
 
 // UPDATING GUEST ON APPLY CLICK
-const guestsApply = booking.card.querySelector('.dropdown--guests .dropdown__content .dropdown__apply');
-guestsApply.addEventListener('click', function () {
-    booking.updateGuests(bookingCard);
-})
-const dateApply = booking.card.querySelector('.dropdown--date .dropdown__content .dropdown__apply');
-const bookingDays = booking.card.querySelector('.booking .booking__days');
-const bookingSumDays = booking.card.querySelector('.booking__sum-total');
-const overAll = booking.card.querySelector('.booking__total').lastElementChild;
-const regExpDays = new RegExp('\\d+\\sсут[а-яё]+')
-dateApply.addEventListener('click', function () {
-    booking.arrival = new Date(Number(arrival[2]), Number(arrival[1]), Number(arrival[0]));
-    booking.departure = new Date(Number(departDate[2]), Number(departDate[1]), Number(departDate[0]));
-    if (booking.getTotalDays() > 1) {
-        bookingDays.textContent = bookingDays.textContent.replace(regExpDays, ' ' + booking.getTotalDays() + ' ' + getWordForm(booking.getTotalDays(), 'сутки', 'суток', 'суток'));
-        bookingSumDays.textContent = spacify(booking.cost*booking.getTotalDays()) + '₽';
-        overAll.textContent = spacify(Number(booking.getTotalCost(booking.getTotalDays())) - booking.discount) + '₽';
-    }
-})
-
-console.log(bookingCard.querySelector('.dropdown--guests .dropdown__select span').textContent)
-
-
+booking.setEventListeners();
 
 // HELPER FUNCTIONS
-function getWordForm(number,form1, form2, form3) {
-    if (number === 1 || (number > 19 && number % 10 === 1)) {
-        return form1;
+    function getWordForm(number, form1, form2, form3) {
+        if (number === 1 || (number > 19 && number % 10 === 1)) {
+            return form1;
+        }
+        if (number > 1 && number < 5) {
+            return form2;
+        } else {
+            return form3;
+        }
     }
-    if (number > 1 && number < 5) {
-        return form2;
+
+    function spacify(number) {
+        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
     }
-    else {
-        return form3;
-    }
-}
-function spacify (number) {
-    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
 }
--- modules/slideshow/__slideshow.js
+++ modules/slideshow/__slideshow.js
@@ -7,49 +7,83 @@ for (let i = 0; i < slideshows.length; i++) {
                       'dots' : slideshow.querySelector('.slideshow__dots').childNodes},
         'slides' : slideshow.querySelectorAll('.slideshow__slide'),
         'currentSlide' : 0,
+        'changeSlide' : function (direction, index) {
+            this.slides[this.currentSlide].classList.remove('active')
+            this.controls.dots[this.currentSlide].classList.remove('active')
+            if (direction === 'show') {
+                this.currentSlide++
+                if (this.currentSlide >= this.slides.length) {
+                    this.currentSlide = 0;
+                }
+            }
+            else {
+                if (direction === 'prev') {
+                    this.currentSlide -= 1;
+                    if (this.currentSlide < 0) {
+                        this.currentSlide = this.slides.length - 1;
+                    }
+                }
+                else if (index >= 0) {
+                    this.currentSlide = index;
+                }
+                else {
+                    this.currentSlide += 1;
+                    if (this.currentSlide >= this.slides.length) {
+                        this.currentSlide = 0;
+                    }
+                }
+            }
+            this.slides[this.currentSlide].classList.add('active')
+            this.controls.dots[this.currentSlide].classList.add('active')
+            if (direction === 'show') {
+                const _this = this;
+                setTimeout(function () { _this.changeSlide('show') }, 2000)
+            }
+        }
     }
     let slider = slideshow.slider;
     slider.slides[slideshow.slider.currentSlide].classList.add('active')
     slider.controls.dots[slideshow.slider.currentSlide].classList.add('active')
     slider.controls.prev.addEventListener('click', function () {
-        changeSlide(slider, 'prev');
+        slider.changeSlide('prev');
     })
     slider.controls.next.addEventListener('click', function () {
-        changeSlide(slider);
+        slider.changeSlide();
     })
     slider.controls.dots.forEach((dot, index) => dot.addEventListener('click', function () {
-        changeSlide(slider, '', index)
+        slider.changeSlide('', index)
     }))
-    let interval = setInterval(changeSlide, 10000, slider, 'show', '')
+    setTimeout(function () { slider.changeSlide('show')}, 10000)
+
 }
 
-function changeSlide(slider, direction, index) {
-    slider.slides[slider.currentSlide].classList.remove('active')
-    slider.controls.dots[slider.currentSlide].classList.remove('active')
-    if (direction === 'show') {
-        slider.currentSlide++
-        if (slider.currentSlide >= slider.slides.length) {
-            slider.currentSlide = 0;
-        }
-    }
-    else {
-        if (direction === 'prev') {
-            slider.currentSlide -= 1;
-            if (slider.currentSlide < 1) {
-                slider.currentSlide = slider.slides.length - 1;
-            }
-        }
-        else if (index >= 0) {
-            slider.currentSlide = index;
-        }
-        else {
-            slider.currentSlide += 1;
-            if (slider.currentSlide >= slider.slides.length) {
-                slider.currentSlide = 0;
-            }
-        }
-    }
-    slider.slides[slider.currentSlide].classList.add('active')
-    slider.controls.dots[slider.currentSlide].classList.add('active')
-}
+// function changeSlide(slider, direction, index) {
+//     slider.slides[slider.currentSlide].classList.remove('active')
+//     slider.controls.dots[slider.currentSlide].classList.remove('active')
+//     if (direction === 'show') {
+//         slider.currentSlide++
+//         if (slider.currentSlide >= slider.slides.length) {
+//             slider.currentSlide = 0;
+//         }
+//     }
+//     else {
+//         if (direction === 'prev') {
+//             slider.currentSlide -= 1;
+//             if (slider.currentSlide < 1) {
+//                 slider.currentSlide = slider.slides.length - 1;
+//             }
+//         }
+//         else if (index >= 0) {
+//             slider.currentSlide = index;
+//         }
+//         else {
+//             slider.currentSlide += 1;
+//             if (slider.currentSlide >= slider.slides.length) {
+//                 slider.currentSlide = 0;
+//             }
+//         }
+//     }
+//     slider.slides[slider.currentSlide].classList.add('active')
+//     slider.controls.dots[slider.currentSlide].classList.add('active')
+// }
 
