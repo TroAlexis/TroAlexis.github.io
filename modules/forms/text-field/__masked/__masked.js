62c69fc833d569ebc5cc68c5003a80618ee29e4d
TroAlexis <aleksei.troshin1997@gmail.com>
2020-08-02 13:02:00 +0300

    Added and configured webpack.

--- modules/forms/text-field/__masked/__masked.js
+++
@@ -1,7 +0,0 @@
-//REQUIRES FORMATTER.JS
-
-
-var masked = new Formatter(document.getElementById('birthdate'), {
-    'pattern' : '{{99}}.{{99}}.{{9999}}',
-    'persistent' : false
-})
--- test.js
+++
@@ -1,4 +0,0 @@
-var masked = new Formatter(document.getElementById('date'), {
-    'pattern' : '{{99}}.{{99}}.{{9999}}',
-    'persistent' : false
-})
