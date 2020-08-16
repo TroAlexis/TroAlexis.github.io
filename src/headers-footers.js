afa8673f0beeb203af2b0ddb84e6be83f3a40cd2
TroAlexis <aleksei.troshin1997@gmail.com>
2020-08-16 16:01:10 +0300

    Headers-footers page mobile adaptation

--- src/headers-footers.js
+++ src/headers-footers.js
@@ -1,3 +1,11 @@
 import './pug/includes/header/header'
+import Header from "./pug/includes/header/header";
+
+const headers = document.querySelectorAll('.header');
+headers.forEach((headerElement) => {
+    const header = new Header(headerElement);
+});
+
 
 import './assets/scss/headers-footers.scss'
+import header from "./pug/includes/header/header";
--- src/pug/includes/header/header.js
+++ src/pug/includes/header/header.js
@@ -1,1 +1,34 @@
 import '../../../js/dAdapt';
+
+const DOM = {
+    burger: '.burger',
+    mobNav: '.header__mob-nav',
+    backDrop: '.backdrop',
+
+};
+
+// INITIALISING HEADER BURGER
+export default class Header {
+    constructor(headerElement) {
+        this.element = headerElement
+        this.burger = headerElement.querySelector(DOM.burger);
+        this.mobNav = headerElement.querySelector(DOM.mobNav);
+        this.backDrop = document.querySelector(DOM.backDrop);
+        this.burger.addEventListener('click', (evt) => {
+            this.toggleMenu();
+        })
+        this.backDrop.addEventListener('click', (evt) => {
+            this.closeMenu();
+        })
+    }
+    toggleMenu() {
+        this.burger.classList.toggle('open');
+        this.mobNav.classList.toggle('open');
+        this.backDrop.classList.toggle('active')
+    }
+    closeMenu() {
+        this.burger.classList.remove('open');
+        this.mobNav.classList.remove('open');
+        this.backDrop.classList.remove('active');
+    }
+}
