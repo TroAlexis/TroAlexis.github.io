5584bae94c8ee419a209a83f71d02e74d62c4d9d
TroAlexis <aleksei.troshin1997@gmail.com>
2020-08-24 15:52:59 +0300

    Fixed button-arrow not showing correctly.

--- src/pug/includes/cards/room-search/roomsearch.js
+++ src/pug/includes/cards/room-search/roomsearch.js
@@ -1,7 +1,7 @@
 import DropdownApply from "../../forms/dropdown/--apply/dropdown--apply";
 import Calendar from "../../forms/calendar/calendar";
 
-const guestsSelectElement = document.querySelector('.roomsearch .dropdown--guests')
+const guestsSelectElement = document.querySelector('.roomsearch .dropdown--apply')
 const guestsSelect = new DropdownApply(guestsSelectElement, ['гость', 'гостя', 'гостей'])
 const calendarElement = document.querySelector('.roomsearch .input-calendar')
 const calendar = new Calendar(calendarElement);
