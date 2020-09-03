5a378e5b644e36a4ce9e759429de77fa5d640548
TroAlexis <aleksei.troshin1997@gmail.com>
2020-09-03 22:54:10 +0300

    Impressions block almost done, added limiting containers.

--- src/room-details.js
+++ src/room-details.js
@@ -1,6 +1,12 @@
 // JS
+import Header from 'Includes/header/header';
 
+const header = new Header(document.querySelector('.header'))
 
 
 // SCSS
 import 'Scss/room-details.scss'
+
+if (module.hot) {
+    module.hot.accept();
+}
